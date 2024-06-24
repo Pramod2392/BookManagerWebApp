a4552e728d201d1fa3439905654cfd82dd45cc49
Pramod Shivaprasad <pramod2392@gmail.com>
2024-06-25 01:21:28 +0530

    Add Book feature implementation

--- BookManagerWebApp/BookManagerWeb/Pages/AddBook.cshtml.cs
+++ BookManagerWebApp/BookManagerWeb/Pages/AddBook.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Identity.Abstractions;
 using System.ComponentModel;
+using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace BookManagerWeb.Pages
@@ -27,25 +28,52 @@ namespace BookManagerWeb.Pages
 
 
         public async Task OnPostAsync()
-        {            
-            using MultipartFormDataContent content = new MultipartFormDataContent();
-            var addBookJson = JsonSerializer.Serialize(addBook);
-
-            await _downstreamApi.PostForUserAsync<string>("DownstreamApiBook", addBookJson);
-            //_downstreamApi.
-            using var response = await _downstreamApi.CallApiForUserAsync("DownstreamApiBook").ConfigureAwait(false);
-
-
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+        {
+            try
             {
-                var apiResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                ViewData["ApiResult"] = apiResult;
+                using MultipartFormDataContent content = new MultipartFormDataContent();
+                var addBookJson = JsonSerializer.Serialize(addBook);
+
+                var requestParameters = new Dictionary<string, string>
+                {
+                    { "Title", addBook.Title },
+                    { "CategoryId", addBook.CategoryId.ToString() },
+                    { "Price", addBook.Price.ToString() }
+                };
+                
+                var fileStream = new MemoryStream();
+                await addBook.Image.CopyToAsync(fileStream);
+                var streamContent = new StreamContent(fileStream);
+                streamContent.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(addBook.Image.ContentType); // Change content type as per your image type
+                content.Add(streamContent, "Image", addBook.Image.FileName); // "image" is the key for the image data
+
+                var response = await _downstreamApi.CallApiForUserAsync("DownstreamApiBook",
+                            options =>
+                            {
+                                options.HttpMethod = HttpMethod.Post.ToString();
+                                options.CustomizeHttpRequestMessage = new Action<HttpRequestMessage>(x =>
+                                {
+                                    x.Content = content;
+                                    x.RequestUri = new Uri($"https://localhost:7264/api/Books?Title={addBook.Title}&CategoryId={addBook.CategoryId}&Price={addBook.Price}");
+                                });                                
+                            });
+
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var apiResult = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    ViewData["ApiResult"] = apiResult;
+                }
+                else
+                {
+                    var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+                    throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
+                };
             }
-            else
+            catch (Exception ex)
             {
-                var error = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
-                throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}: {error}");
-            };            
+                _logger.LogError($"Error: {ex.Message}");
+                throw;
+            }           
         }
     }
 }
