6a0384d7d67c3246782a828f82c6d4dcaccde3f8
Pramod Shivaprasad <pramod2392@gmail.com>
2024-11-30 18:45:01 +0530

    converted the file name to .jpeg

--- BookManagerWebApp/BookManagerWeb/Pages/AddBook.cshtml.cs
+++ BookManagerWebApp/BookManagerWeb/Pages/AddBook.cshtml.cs
@@ -90,7 +90,7 @@ namespace BookManagerWeb.Pages
                         var imageBytes = client.GetByteArrayAsync(ImageURL).Result;
 
                         var stream = new MemoryStream(imageBytes);
-                        addBook.Image = new FormFile(stream, 0, imageBytes.Length, "ImageFile", "filename.jpg") 
+                        addBook.Image = new FormFile(stream, 0, imageBytes.Length, "ImageFile", "filename.jpeg") 
                                                 { 
                                                     ContentType = "image/jpeg"
                                                 };
